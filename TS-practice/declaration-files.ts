183acddada9c0c57ffc5acaa548ed6ac72f9ff28
angiegigishang <shangxingyu@163.com>
2023-04-03 23:20:06 +0800

    add ts practice

---
+++ TS-practice/declaration-files.ts
@@ -0,0 +1,13 @@
+// axios('text.url')
+// axios1.get('url')
+// axios1.post('url', {name: 'viking'})
+
+import axios from 'axios';
+import calculator from './caculator';
+//axios.post
+
+//calculator('plus', [1, 2])
+//calculator.plus([2, 1])
+
+//calculator('minus', [1, 2])
+calculator.minus([1,2])
---
+++ TS-practice/text1.ts
@@ -0,0 +1,145 @@
+function add(x: number, y: number, z: number = 10): number {
+   if (typeof z === 'number') {
+     return x + y + z
+   } else {
+      return x + y 
+   }
+ }
+ 
+ let result = add(2, 3, 5)
+
+ const add2 = function(x: number, y: number, z:number = 10): number {
+    if (typeof z === 'number') {
+        return  x + y + z
+    } else {
+        return x + y
+    }
+ }
+
+ const add3: (x: number, y: number, z: number) => number = add2
+
+enum Direction {
+    Up,
+    Down,
+    Left ,
+    Right = "RIGHT"
+}
+// const value = 'UP'
+// if (value === Direction.Up) {
+//     console.log('go up!')
+// }
+
+function echo<T>(arg: T): T {
+    return arg
+}
+const str: string = 'str'
+const results = echo(str)
+const results1 = echo(123)
+const results2 = echo(true)
+
+function swap<T, U>(tuple: [T, U]): [U, T] {
+    return [tuple[1], tuple[0]]
+}
+const results3 = swap(['string', 123])
+results3[1].concat
+
+function echoWithArr<T>(arg: T[]): T[] {
+    console.log(arg.length)
+    return arg
+}
+const arrs= echoWithArr([1,2,3])
+
+interface IwithLength {
+    length: number
+}
+
+function echoWithLength<T extends IwithLength>(arg: T):T {
+    console.log(arg.length)
+    return arg
+}
+
+const strss = echoWithLength('str')
+const obj = echoWithLength({ length: 10, width: 10 })
+const arr2 = echoWithLength([1, 2, 3])
+
+class Queue<T> {
+    private data = [];
+    push(item: T) {
+        return this.data.push(item)
+    }
+    pop():T {
+        return this.data.shift()
+    }
+}
+
+const queue = new Queue<number>()
+queue.push(1)
+console.log(queue.pop().toFixed())
+console.log(queue.pop().toFixed())
+
+const queue2 = new Queue<string>()
+queue2.push('str')
+console.log(queue2.pop().length)
+
+interface KeyPair<T, U> {
+    key: T;
+    value: U
+}
+let kp1: KeyPair<number, string> = { key: 1, value: 'str' }
+let kp2: KeyPair<string,number> = { key: 'text', value: 23 }
+
+let arr: number[] = [1, 2, 3]
+let arrTwo: Array<number> = [1, 2, 3]
+
+let arr1: number[] = [1,2,3]
+let arrTwo1: Array<number> = [1, 2, 3]
+
+interface IPlus<T> {
+    (a: T, b: T): T
+}
+
+interface IPlus2<T> {
+    (a: T, b: T): T
+}
+function plus(a: number, b: number): number {
+    return a + b
+}
+function connect(a: string, b: string): string {
+    return a + b
+}
+const a: IPlus<number> = plus
+const b: IPlus<string> = connect
+
+
+type PlusType = (x: number, y: number) => number
+
+function sum1(x: number, y: number): number {
+    return x + y
+}
+const sum2: (x: number, y: number) => number = sum1
+const sum3: PlusType = sum1;
+
+type NameResolver = () => string
+type NameOrResover = string | NameResolver
+function getName(n: NameResolver): string {
+    if(typeof n === 'string') {
+        return n
+    } else {
+        return n()
+    }
+}
+
+function getLength(input: string | number): number {
+    // const str = input as String
+    // if (str.length) {
+    //     return str.length
+    // } else {
+    //     const number = input as Number
+    //     return number.toString().length
+    // }
+    if((<string>input).length) {
+        return (<string>input).length
+    } else {
+        return input.toString().length
+    }
+}
