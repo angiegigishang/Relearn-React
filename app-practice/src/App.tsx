871360650d9761948a9d485f798c44a63e29af37
ashang1 <angie.shang@activenetwork.com>
2023-05-15 20:22:00 +0800

    add useContext

--- app-practice/src/App.tsx
+++ app-practice/src/App.tsx
@@ -12,36 +12,56 @@ interface IShowResult {
   status: string;
 }
 
+interface IThemeProps {
+  [key: string]: {color: string; background: string;}
+}
+const themes :IThemeProps = {
+  'light': {
+    color: '#000',
+    background: '#eee'
+  },
+  'dark': {
+    color: '#fff',
+    background: '#222'
+  }
+}
+
+export const ThemeContext = React.createContext(themes.light)
+
 function App() {
-  const [ show, setShow ] = useState(true)
+  //const [ show, setShow ] = useState(true)
   //const [ data, loading] = useURLLoader('https://dog.ceo/api/breeds/image/random', [show])
-  const [ data, loading] = useURLLoader('https://dog.ceo/api/breeds/image/random', [show])
-  const dogResult = data as IShowResult
-  const positions = useMousePosition()
+  //const [ data, loading] = useURLLoader('', [show])
+ // const dogResult = data as IShowResult
+  //const positions = useMousePosition()
   return (
     <div className="App">
+      <ThemeContext.Provider value={themes.dark}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
+        </p> */}
+        {/* <p>
           <button onClick={() => {setShow(!show)}}>Refresh dog photo</button>
         </p>
         { loading ? <p>🐶 is reading...</p>
-        : <img src={dogResult && dogResult.message}/>}
-        { show && <MouseTracker/> }
-        <p>x: {positions.x}, y: {positions.y}</p>
+        : <img src={dogResult && dogResult.message}/>} */}
+        {/* { show && <MouseTracker/> }
+        <p>x: {positions.x}, y: {positions.y}</p> */}
         <LikeButton/>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        
       </header>
+      </ThemeContext.Provider>
     </div>
   );
 }
