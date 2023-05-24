2584627dd9ce4ecdd02aa03b64d71abf26b0e04d
angiegigishang <shangxingyu@163.com>
2023-05-24 20:09:00 +0800

    solve errors and update button component

--- app-practice/src/App.tsx
+++ app-practice/src/App.tsx
@@ -6,11 +6,11 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <Button> hello</Button>
-        <Button disabled btnType={ButtonType.Primary} size={ButtonSize.Large}> hello </Button>
-        <Button btnType={ButtonType.Link} href="http://www.baidu.com">hello</Button>
-        <Button size={ButtonSize.Large}>dfsfd</Button>
-        <Button size={ButtonSize.Small}>dfsfd</Button>
+        <Button onClick={(e) => {e.preventDefault(); alert('123')}}> hello</Button>
+        <Button disabled btnType={ButtonType.Primary} size={ButtonSize.Large}> Disabled Button </Button>
+        <Button size={ButtonSize.Large}>Large Button</Button>
+        <Button size={ButtonSize.Small}>Small Button</Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com" target="_blank">Disabled Link</Button>
       </header>
     </div>
   );
--- app-practice/src/components/Button/button.tsx
+++ app-practice/src/components/Button/button.tsx
@@ -22,19 +22,21 @@ interface BaseButtonProps {
   href?: string;
 }
 
-type NativeButtonProps = BaseButtonProps & React.BaseHTMLAttributes<HTMLElement>
+type NativeButtonProps = BaseButtonProps & React.ButtonHTMLAttributes<HTMLElement>
 type AnchorButtonProps = BaseButtonProps & React.AnchorHTMLAttributes<HTMLElement>
 export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
 
 const Button: React.FC<ButtonProps> = (props) => {
   const {
     btnType,
+    className,
     disabled,
     size,
     children,
-    href
+    href,
+    ...restProps
   } = props
-  const classes = classNames('btn', {
+  const classes = classNames('btn', className, {
     [`btn-${btnType}`]: btnType,
     [`btn-${size}`]: size,
     'disabled': (btnType === ButtonType.Link) && disabled
@@ -44,6 +46,7 @@ const Button: React.FC<ButtonProps> = (props) => {
       <a
           className={classes}
           href={href}
+          {...restProps}
       >
         {children}
       </a>
@@ -53,6 +56,7 @@ const Button: React.FC<ButtonProps> = (props) => {
       <button
         className={classes}
         disabled={disabled}
+        {...restProps}
       >
         {children}
       </button>
