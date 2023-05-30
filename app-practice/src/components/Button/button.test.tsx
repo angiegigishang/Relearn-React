0b624d2ca54fa19f639f572f403faeda1409b1b2
angiegigishang <shangxingyu@163.com>
2023-05-30 19:23:59 +0800

    add Button Unit Test

--- app-practice/src/components/Button/button.test.tsx
+++ app-practice/src/components/Button/button.test.tsx
@@ -12,26 +12,41 @@ const testProps: ButtonProps = {
   className: 'klass'
 }
 
+const disabledProps: ButtonProps = {
+  disabled: true,
+  onClick: jest.fn()
+}
+
 describe('test Button component', () => {
   it('should render the correct default button', () => {
     const wrapper = render(<Button {...defaultProps}>Nice</Button>)
-    const element = wrapper.getByText('Nice')
+    const element = wrapper.getByText('Nice') as HTMLButtonElement
     expect(element).toBeInTheDocument()
     expect(element.tagName).toEqual('BUTTON')
     expect(element).toHaveClass('btn btn-default')
     fireEvent.click(element)
     expect(defaultProps.onClick).toHaveBeenCalled()
+    expect(element.disabled).toBeFalsy()
   })
   it('should render the correct component based on different props', () => {
-    const wrapper = render(<Button {...defaultProps}>Nice</Button>)
+    const wrapper = render(<Button {...testProps}>Nice</Button>)
     const element = wrapper.getByText('Nice')
     expect(element).toBeInTheDocument()
-    expect(element).toHaveClass('btn-primary btn-lg')
+    expect(element).toHaveClass('btn-primary btn-lg klass')
   })
   it('should render a link when btnType equals link and href is provided', () => {
-
+    const wrapper = render(<Button btnType={ButtonType.Link} href="http://www.baidu.com">Link</Button>)
+    const element = wrapper.getByText('Link')
+    expect(element).toBeInTheDocument()
+    expect(element.tagName).toEqual('A')
+    expect(element).toHaveClass('btn btn-link')
   })
   it('should render disabled button when disabled set is true', () => {
-
+    const wrapper = render(<Button {...disabledProps}>Nice</Button>)
+    const element = wrapper.getByText('Nice') as HTMLButtonElement
+    expect(element).toBeInTheDocument()
+    expect(element.disabled).toBeTruthy()
+    fireEvent.click(element)
+    expect(disabledProps.onClick).not.toHaveBeenCalled()
   })
 })
