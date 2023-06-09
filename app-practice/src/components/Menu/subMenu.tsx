5f8c2e53f44574459abd549c6d451a5e42e75e3c
angiegigishang <shangxingyu@163.com>
2023-06-09 19:24:58 +0800

    Add original and simple CSS animation style to react component

--- app-practice/src/components/Menu/subMenu.tsx
+++ app-practice/src/components/Menu/subMenu.tsx
@@ -2,6 +2,7 @@ import React, { useContext, useState, FunctionComponentElement } from 'react';
 import classNames from 'classnames';
 import { MenuContext } from './menu';
 import { MenuItemProps } from './menuItem';
+import Icon from "../Icon/icon";
 
 export interface SubMenuProps {
   index?: string;
@@ -16,7 +17,9 @@ const SubMenu: React.FC<SubMenuProps> = ({ index, title, children, className}) =
   const isOpened = (index && context.mode === 'vertical') ? openedSubMenus.includes(index) : false
   const [ menuOpen, setOpen ] = useState(isOpened)
   const classes = classNames('menu-item submenu-item', className, {
-    'is-active': context.index === index
+    'is-active': context.index === index,
+    'is-opened': menuOpen,
+    'is-vertical': context.mode = 'vertical'
   })
   const handleClick = (e: React.MouseEvent) => {
     e.preventDefault()
@@ -66,6 +69,7 @@ const SubMenu: React.FC<SubMenuProps> = ({ index, title, children, className}) =
     <li key={index} className={classes} {...hoverEvents}>
       <div className="submenu-title" {...clickEvents} onClick={handleClick}>
        {title}
+       <Icon icon="angle-down" className="arrow-icon"/>
       </div>
       {renderChildren()}
     </li>
