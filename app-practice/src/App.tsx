647513e5b3918cf25a9fb33c93ffb89fdcfad990
angiegigishang <shangxingyu@163.com>
2023-07-19 19:10:56 +0800

    Mock server and basic axios example

--- app-practice/src/App.tsx
+++ app-practice/src/App.tsx
@@ -1,98 +1,35 @@
-import React, { useState } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas, faCoffee } from '@fortawesome/free-solid-svg-icons';
-import Button, { ButtonType, ButtonSize} from "./components/Button/button";
-import Alert, {AlertType} from "./components/Alert/alert";
-import Menu from "./components/Menu/menu";
-import MenuItem from "./components/Menu/menuItem";
-import SubMenu from "./components/Menu/subMenu";
-import Practice from "./components/Practice/practice";
-import Icon from "./components/Icon/icon";
-import Transition from "./components/Transition/transition";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-library.add(fas)
-
-//function App() {
 const App: React.FC = () => {
-  const [ show, setShow ] = useState(false)
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Practice></Practice>
-        <FontAwesomeIcon icon={faCoffee} size="lg" spin></FontAwesomeIcon>
-        <Icon icon="arrow-down" theme="primary" size="10x"/>
-        <Menu defaultIndex='0' 
-              onSelect={(index) => {alert('menu-test' + index)}} 
-              mode="vertical"
-              defaultOpenSubMenus={['2']}
-        >
-          <MenuItem>
-            cool link 1
-          </MenuItem>
-          <MenuItem disabled>
-            cool link 2
-          </MenuItem>
-          <SubMenu title="dropdown">
-            <MenuItem>
-              dropdown1
-            </MenuItem>
-            <MenuItem>
-              dropdown2
-            </MenuItem>
-            <MenuItem>
-              dropdown3
-            </MenuItem>
-          </SubMenu>
-          <MenuItem>
-            cool link 3
-          </MenuItem>
-        </Menu>
-        <Button size="lg" onClick={() => {setShow(!show)}}>Toggle</Button>
-        <Transition
-          in={show}
-          timeout={300}
-          animation="zoom-in-left"
-        >
-          <div>
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-          </div>
-        </Transition>
-        <Transition
-          in={show}
-          timeout={300}
-          animation="zoom-in-left"
-          wrapper
-        >
-          <Button btnType="primary" size="lg">A Large Button</Button>
-        </Transition>
-        
-        {/* <Button onClick={(e) => {e.preventDefault(); alert('123')}}> hello</Button>
-        <Button disabled btnType={ButtonType.Primary} size={ButtonSize.Large}> Disabled Button </Button>
-        <Button size={ButtonSize.Large}>Large Button</Button>
-        <Button size={ButtonSize.Small}>Small Button</Button>
-        <Button btnType={ButtonType.Link} href="http://www.baidu.com" target="_blank">Disabled Link</Button> */}
+  const [ title, setTitle] = useState('')
+  const postData = {
+    title: 'my title',
+    body: 'hello man'
+  }
+  useEffect(() => {
+    // axios.get('https://jsonplaceholder.typicode.com/posts/1', {
+    //   headers: {
+    //     'X-Requested-Width': 'XMLHttpRequest',
+    //   },
+    //   responseType: 'json'
+    // })
+    //   .then(resp => {
+    //     setTitle(resp.data.title)
+    //   })
+    axios.post('https://jsonplaceholder.typicode.com/posts', postData)
+      .then(resp => {
+        console.log(resp)
+        setTitle(resp.data.title)
+      })
+  })
+  return(
+    <div className='App'>
+      <header className='App-header'>
+        <h1>{title}</h1>
       </header>
-      {/* <div>
-        <Alert alertType="success" content="alert1" header="header1"></Alert>
-        <Alert alertType="danger" content="alert2"></Alert>
-      </div> */}
     </div>
-  );
+  )
 }
 
 export default App;
